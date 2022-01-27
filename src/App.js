5aaa90d6a81731f9276a91dc821608af42328a2d
João Souza <56441159+joaomaarinho@users.noreply.github.com>
2022-01-27 13:06:37 -0300

    introduction
    first course module

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import PostCreate from "./component/PostCreate";
+import PostList from "./component/PostList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1>Blog App</h1>
+        <PostCreate />
+        <hr />
+        <h1>POSTS</h1>
+        <PostList />
+      </div>
   );
 }
 
